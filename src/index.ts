e17b144f51b13665fd2689dc2eaed8bd0be51301
C0C41N <0ct41n@gmail.com>
2020-07-15 17:51:36 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.raw())
 
 app.post('/.netlify/functions/main', async (req: Request, res: Response) => {
-	const body = req.body
+	const body = req.body.toString()
 
 	console.log(JSON.stringify(body, null, 3))
 	res.end(JSON.stringify(body, null, 3))

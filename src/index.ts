a924579137a86cf614b131c98809e6a1937b7490
C0C41N <0ct41n@gmail.com>
2020-07-15 21:59:48 +0500

    BLAH

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { Request, Response } from 'express'
 const app = express()
 
 app.post('/.netlify/functions/main', async (req: Request, res: Response) => {
-	const body = JSON.parse(req.body.toString().replace(/[\\]/g, ''))
+	const body = JSON.parse(req.body.toString().replace(/\\/g, ''))
 
 	console.log(JSON.stringify(body, null, 3))
 	res.end(JSON.stringify(body, null, 3))

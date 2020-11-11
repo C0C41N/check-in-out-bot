e1337adf39b63e2add714186b09d843e3e167340
C0C41N <0ct41n@gmail.com>
2020-11-11 07:47:03 +0500

    disabled

--- src/func/checkIn.ts
+++ src/func/checkIn.ts
@@ -1,99 +1,99 @@
-import { db, getSheets } from '../api'
-import { MyUserId } from '../ts/const'
-import { ISheetAppendResp } from '../ts/types'
-import { checkDriveStructure } from './checkDriveStructure'
-import { getAgentDB, IAgentDB } from './getAgentDb'
-import { getProfile, IUserProfile } from './getProfile'
-import { getRandomNumber } from './getRandomKey'
-import { getTimestamp } from './getTimestamp'
-import { replyToAgent } from './replyToAgent'
-import { sendPushMsg } from './sendPushMsg'
+import { db, getSheets } from '../api';
+import { MyUserId } from '../ts/const';
+import { ISheetAppendResp } from '../ts/types';
+import { checkDriveStructure } from './checkDriveStructure';
+import { getAgentDB, IAgentDB } from './getAgentDb';
+import { getProfile, IUserProfile } from './getProfile';
+import { getRandomNumber } from './getRandomKey';
+import { getTimestamp } from './getTimestamp';
+import { replyToAgent } from './replyToAgent';
+import { sendPushMsg } from './sendPushMsg';
 
 // prettier-ignore
 export async function checkIn(userId: string, groupId: string, replyToken: string) {
 
-	const agent = await getAgentDB(userId)
+	// const agent = await getAgentDB(userId)
 
-	if (agent === false) {
+	// if (agent === false) {
 
-		return await doCheckIn()
-	}
+	// 	return await doCheckIn()
+	// }
 
-	else {
+	// else {
 
-		if (agent.sheetId) {
+	// 	if (agent.sheetId) {
 
-			const { displayName } = await profile()
+	// 		const { displayName } = await profile()
 
-			const msg = `${displayName},\n\nYou're already Checked In.`
+	// 		const msg = `${displayName},\n\nYou're already Checked In.`
 
-			await replyToAgent(replyToken, msg)
+	// 		await replyToAgent(replyToken, msg)
 
-			return msg
-		}
+	// 		return msg
+	// 	}
 
-		else {
-			return await doCheckIn()
-		}
-	}
+	// 	else {
+	// 		return await doCheckIn()
+	// 	}
+	// }
 
-	async function doCheckIn() {
+	// async function doCheckIn() {
 
-		const { date, time } = getTimestamp()
+	// 	const { date, time } = getTimestamp()
 
-		const { displayName } = await profile()
+	// 	const { displayName } = await profile()
 
-		const msg = [
-			`Check-In successful!\n`,
-			`Name: ${displayName}`,
-			`Date: ${date}`,
-			`Time: ${time}`
-		].join('\n')
+	// 	const msg = [
+	// 		`Check-In successful!\n`,
+	// 		`Name: ${displayName}`,
+	// 		`Date: ${date}`,
+	// 		`Time: ${time}`
+	// 	].join('\n')
 
-		await replyToAgent(replyToken, msg)
+	// 	await replyToAgent(replyToken, msg)
 
-		const sheetId = await checkDriveStructure(date)
-		const sheets = await getSheets()
-		const name = agent && agent.realName ? agent.realName : displayName
+	// 	const sheetId = await checkDriveStructure(date)
+	// 	const sheets = await getSheets()
+	// 	const name = agent && agent.realName ? agent.realName : displayName
 
-		const resp = await sheets.spreadsheets.values.append({
-			range: 'main!B8:F8',
-			requestBody: {
-				values: [[ date, name, time ]],
-			},
-			spreadsheetId: sheetId,
-			valueInputOption: 'USER_ENTERED',
-		}) as ISheetAppendResp
+	// 	const resp = await sheets.spreadsheets.values.append({
+	// 		range: 'main!B8:F8',
+	// 		requestBody: {
+	// 			values: [[ date, name, time ]],
+	// 		},
+	// 		spreadsheetId: sheetId,
+	// 		valueInputOption: 'USER_ENTERED',
+	// 	}) as ISheetAppendResp
 
-		const { updatedRange: range } = resp.data.updates
+	// 	const { updatedRange: range } = resp.data.updates
 
-		const Agent: IAgentDB = { displayName, range, sheetId }
+	// 	const Agent: IAgentDB = { displayName, range, sheetId }
 
-		let msg2
+	// 	let msg2
 
-		if (!(agent && agent.handle)) {
-			const handle = getRandomNumber(4)
-			Agent.handle = handle
+	// 	if (!(agent && agent.handle)) {
+	// 		const handle = getRandomNumber(4)
+	// 		Agent.handle = handle
 
-			msg2 = [
-				`Agent First-time Check-IN\n`,
-				`Name: ${displayName}`,
-				`Time: ${time}\n`,
-				`HANDLE: ${handle}\n`,
-				`Please enter real name for this agent\n`,
-				`reply like this: #rename HANDLE = REAL NAME`,
-			].join('\n')
+	// 		msg2 = [
+	// 			`Agent First-time Check-IN\n`,
+	// 			`Name: ${displayName}`,
+	// 			`Time: ${time}\n`,
+	// 			`HANDLE: ${handle}\n`,
+	// 			`Please enter real name for this agent\n`,
+	// 			`reply like this: #rename HANDLE = REAL NAME`,
+	// 		].join('\n')
 
-			await sendPushMsg(MyUserId, msg2)
-		}
+	// 		await sendPushMsg(MyUserId, msg2)
+	// 	}
 
-		await db.ref(`agents/${userId}`).update(Agent)
+	// 	await db.ref(`agents/${userId}`).update(Agent)
 
-		return `${msg}\n\n			---			\n\n${msg2}`
-	}
+	// 	return `${msg}\n\n			---			\n\n${msg2}`
+	// }
 
-	async function profile() {
+	// async function profile() {
 
-		return await getProfile(userId, groupId) as IUserProfile
-	}
+	// 	return await getProfile(userId, groupId) as IUserProfile
+	// }
 }
